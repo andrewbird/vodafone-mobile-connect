bb293c1a017a58bbd5ff6846dd3677f4d01af439
Andrew Bird <ajb@spheresystems.co.uk>
2008-11-14 15:04:02 +0000

    Add platform support for vfon1

---
+++ src/vodafone-mobile-connect/resources/platform/vfon1/usr/sbin/vmc-option.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+VID="$1"
+PID="$2"
+OP="$3"
+
+PATH=/usr/bin:/bin:/usr/sbin:/sbin
+
+
+case "${OP}" in
+	flip)	# special case for flipping ZeroCD to modem mode
+		modprobe -r option
+		modprobe -r pl2303
+		modprobe -r usbserial
+		usb_modeswitch -v ${VID} -p ${PID} \
+			-m 0x05 -M 55534243123456780000000000000601000000000000000000000000000000
+			# need to move to using ozerocdoff for better discovery
+
+		;;
+
+	*)	# default action is to load option driver with supplied IDs
+		modprobe -a option
+		echo "0x${VID} 0x${PID}" > /sys/bus/usb-serial/drivers/option1/new_id
+		;;
+esac
+
+
