f26b65dd11a8ff347b293b86ba5aeb82307bbcc4
Andrew Bird <ajb@spheresystems.co.uk>
2008-11-14 10:54:12 +0000

    Add new ZTE plugins - k3520-z, k3565-z

---
+++ src/vodafone-mobile-connect/plugins/devices/zte_k3565.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2006-2007  Vodafone España, S.A.
+# Author:  Pablo Martí
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+__version__ = "$Rev: 1209 $"
+
+from vmc.common.exceptions import DeviceLacksExtractInfo
+from vmc.common.hardware.zte import ZTECustomizer
+from vmc.common.plugin import DBusDevicePlugin
+
+class ZTEK3565(DBusDevicePlugin):
+    """L{vmc.common.plugin.DBusDevicePlugin} for ZTE's version of Vodafone's K3565"""
+    name = "Vodafone K3565-Z"
+    version = "0.1"
+    author = "Andrew Bird"
+    custom = ZTECustomizer
+    
+    __remote_name__ = "K3565-Z"
+
+    __properties__ = {
+        'usb_device.vendor_id': [0x19d2],
+        'usb_device.product_id': [0x0049, 0x0052], # depends on firmware version
+    }
+    
+    def extract_info(self, children):
+        # K3565-Z uses ttyUSB2(data) and ttyUSB1(status)
+        for device in children:
+            try:
+                if device['serial.port'] == 1: # control port
+                    self.cport = device['serial.device'].encode('utf8')
+                elif device['serial.port'] == 2: # data port
+                    self.dport = device['serial.device'].encode('utf8')
+            except KeyError:
+                pass
+        
+        if not self.cport or not self.dport:
+            raise DeviceLacksExtractInfo(self)
+
+zte_k3565= ZTEK3565()
+
