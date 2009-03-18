6d30aaa208b419a95b2070181b788a4b696e402e
Andrew Bird <ajb@spheresystems.co.uk>
2009-03-18 19:17:53 +0000

    AAone - update ZTE platform collateral

--- src/vodafone-mobile-connect/resources/platform/aaone/usr/sbin/vmc-settle-eject.sh
+++ src/vodafone-mobile-connect/resources/platform/aaone/usr/sbin/vmc-settle-eject.sh
@@ -6,23 +6,18 @@
 DEVICE="$1"
 WAIT="$2"
 
-LOG=/var/log/vmc-settle-eject.log
-
-echo called at "`date`" > ${LOG}
-
 PATH=/usr/bin:/bin:/usr/sbin:/sbin
 
-# eject device ASAP
-eject ${DEVICE} >> S{LOG} 2>&1
+(
+        for i in `seq ${WAIT}` ; do
+		if [ -d /sys/bus/usb-serial/drivers/option1/ttyUSB1 ] ; then
+			exit 0
+		fi
 
-( 
-   sleep ${WAIT}
+		eject ${DEVICE}
 
-   if [ ! -e /dev/ttyUSB1 ] ; then 
-      echo /dev/ttyUSB1 not found after ${WAIT} seconds >> ${LOG}
-      echo ejecting again >> ${LOG}
-      eject ${DEVICE} >> S{LOG} 2>&1
-   fi
+		sleep 1
+	done
 
 ) < /dev/null > /dev/null 2>&1 &
 
--- src/vodafone-mobile-connect/resources/platform/aaone/usr/sbin/vmc-zte.sh
+++ src/vodafone-mobile-connect/resources/platform/aaone/usr/sbin/vmc-zte.sh
@@ -5,9 +5,8 @@ PID="$2"
 
 PATH=/usr/bin:/bin:/usr/sbin:/sbin
 
-
 # it may not use this driver if the product id is already 
 # compiled into another driver, it's best to blacklist pl2303 and onda
-modprobe -a option
+[ -f /sys/bus/usb-serial/drivers/option1/new_id ] || modprobe -a option
 echo "0x${VID} 0x${PID}" > /sys/bus/usb-serial/drivers/option1/new_id
 
