bdff52e66e98f646c2c2ccdf52adf916527df6f8
Andrew Bird <ajb@spheresystems.co.uk>
2009-05-06 19:27:41 +0000

    Add support for Huawei E510

---
+++ src/vodafone-mobile-connect/plugins/devices/huawei_e510.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2006-2009  Vodafone España, S.A.
+# Author:  Andrew Bird
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+__version__ = "$Rev: 1172 $"
+
+from vmc.common.hardware.huawei import HuaweiCustomizer
+from vmc.common.plugin import DBusDevicePlugin
+
+
+class HuaweiE510(DBusDevicePlugin):
+    """L{vmc.common.plugin.DBusDevicePlugin} for Huawei's E510"""
+    name = "Huawei E510"
+    version = "0.1"
+    author = u"Andrew Bird"
+    custom = HuaweiCustomizer
+    
+    __remote_name__ = "E510"
+
+    __properties__ = {
+        'usb_device.vendor_id': [0x12d1],
+        'usb_device.product_id': [0x1411],
+    }
+
+    hardcoded_ports = (0,2)
+
+huaweie510 = HuaweiE510()
+    
