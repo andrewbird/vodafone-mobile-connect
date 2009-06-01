5d601e7020650b9d1ad06068e25e69703284e772
Andrew Bird <ajb@spheresystems.co.uk>
2009-06-01 14:41:07 +0000

    EeePC - Novatel device collateral update

--- src/vodafone-mobile-connect/resources/platform/eeepc/usr/sbin/vmc-novatel.sh
+++ src/vodafone-mobile-connect/resources/platform/eeepc/usr/sbin/vmc-novatel.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-# for Ovation device
-/sbin/modprobe -r option
-eject /dev/novatel-storage
+VID="$1"
+PID="$2"
 
-/sbin/modprobe -a option
-echo "0x1410 0x4400" > /sys/bus/usb-serial/drivers/option1/new_id
+PATH=/usr/bin:/bin:/usr/sbin:/sbin
+
+modprobe -a option
+echo "0x${VID} 0x${PID}" > /sys/bus/usb-serial/drivers/option1/new_id
 
