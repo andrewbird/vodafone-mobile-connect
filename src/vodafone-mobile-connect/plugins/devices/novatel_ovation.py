78997e834d37a6bd349f52297f031533079440d3
Andrew Bird <ajb@spheresystems.co.uk>
2009-07-01 16:31:35 +0000

    Novatel - MC950D doesn't need network registration hack

--- src/vodafone-mobile-connect/plugins/devices/novatel_ovation.py
+++ src/vodafone-mobile-connect/plugins/devices/novatel_ovation.py
@@ -17,72 +17,16 @@
 
 __version__ = "$Rev: 1172 $"
 
-from twisted.python import log
 import serial
-import vmc.common.exceptions as ex
 from vmc.common.hardware.novatel import NovatelCustomizer, NovatelDBusDevicePlugin
-from vmc.common.statem.networkreg import NetworkRegStateMachine
-
-from vmc.contrib.epsilon.modal import mode
-
-
-class OvationNetworkRegStateMachine(NetworkRegStateMachine):
-    """
-    NetworkRegStateMachine for NovatelWireless' Ovation
-    
-    The Ovation doesn't seems to report via +CREG its network registration
-    status, so the vanilla NetworkRegStateMachine wont work. Instead we
-    shortcut the process and move directly to obtain_netinfo
-    """
-    wait_to_register = NetworkRegStateMachine.wait_to_register
-    obtain_netinfo = NetworkRegStateMachine.obtain_netinfo
-    search_operators = NetworkRegStateMachine.search_operators
-    international_roaming = NetworkRegStateMachine.international_roaming
-    register_with_operator = NetworkRegStateMachine.register_with_operator
-    registration_finished = NetworkRegStateMachine.registration_finished
-    registration_failed = NetworkRegStateMachine.registration_failed
-    
-    class check_registered(mode):
-        def __enter__(self):
-            pass
-        def __exit__(self):
-            pass
-        
-        def do_next(self):
-            log.msg("%s: NEW MODE: check_registered" % self)
-            self.device.sconn.set_charset("IRA")
-            self.device.sconn.set_network_info_format() # set it to numeric
-            
-            def get_netinfo_cb(netinfo):
-                # Novatel Ovation doesn't reports +CREG notifications so we
-                # have to modify its netreg process, we will query directly
-                # the network is registered with
-                log.msg("%s: NEW MODE: obtain_netinfo" % self)
-                d = self.device.sconn.get_imsi()
-                d.addCallback(lambda response: int(response[:5])) # FIXME - IMSI 5 digit issue
-                d.addCallback(self._process_imsi_cb)
-            
-            def get_netinfo_eb(failure):
-                failure.trap(ex.NetworkTemporalyUnavailableError)
-                d = self.device.sconn.get_netreg_status()
-                d.addCallback(self._process_netreg_status)
-                d.addErrback(log.err)
-            
-            d = self.device.sconn.get_network_info(process=False)
-            d.addCallback(get_netinfo_cb)
-            d.addErrback(get_netinfo_eb)
-
-
-class NovatelOvationCustomizer(NovatelCustomizer):
-    netrklass = OvationNetworkRegStateMachine
-
 
 class NovatelOvation(NovatelDBusDevicePlugin):
     """L{vmc.common.plugin.DBusDevicePlugin} for Novatel's Ovation"""
     name = "Novatel MC950D"
     version = "0.1"
     author = u"Pablo Martí"
-    custom = NovatelOvationCustomizer
+    #custom = NovatelOvationCustomizer
+    custom = NovatelCustomizer
 
     __remote_name__ = "Ovation MC950D Card"
 
