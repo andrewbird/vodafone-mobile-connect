2022117b6c0cd167deb6c2c3ccd0038c5deab882
Andrew Bird <ajb@spheresystems.co.uk>
2010-02-01 21:36:13 +0000

    VMC 2.x - Fedora platform update for Huawei devices

--- src/vodafone-mobile-connect/resources/platform/fedora/usr/sbin/vmc-huawei.sh
+++ src/vodafone-mobile-connect/resources/platform/fedora/usr/sbin/vmc-huawei.sh
@@ -2,13 +2,18 @@
 
 VID="$1"
 PID="$2"
+SWT="$3"
 
 PATH=/usr/bin:/bin:/usr/sbin:/sbin
 
+modprobe -r airprime
 modprobe -r option
 modprobe -r pl2303
 modprobe -r usbserial
-usb_modeswitch -v ${VID} -p ${PID} -H 1
+
+if [ "${SWT}" = "yes" ] ; then
+    usb_modeswitch -v ${VID} -p ${PID} -H 1
+fi
 
 # may not use this driver if the product id is already compiled into another driver
 modprobe -a option
