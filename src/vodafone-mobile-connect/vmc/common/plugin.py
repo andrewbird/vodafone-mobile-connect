a4b503bd1017d755fbe3221aa80a9d0155579ad6
Andrew Bird <ajb@spheresystems.co.uk>
2010-04-20 20:11:44 +0000

    Bump version numbers to 2.25.01

--- src/vodafone-mobile-connect/vmc/common/plugin.py
+++ src/vodafone-mobile-connect/vmc/common/plugin.py
@@ -37,7 +37,7 @@ import vmc.common.interfaces as interfaces
 from vmc.common.runtime import app_is_frozen
 from vmc.common.sim import SIMBaseClass
 
-VERSION = 2.201
+VERSION = 2.251
 
 BASE_PATH_DICT = {
       'WVDIAL_AUTH_CONF': WVDIAL_AUTH_CONF,
